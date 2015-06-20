e98a2b20d7842513f50fb7521f4fbdf0dbb3a8f1
Kevin Houk <kmhouk@gmail.com>
2015-06-20 15:54:33 -0400

    fixed slider and added tests

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -4,4 +4,15 @@ class ContactsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Load form submission" do
+    get :new
+    assert_response :success
+  end
+
+  test "Create form submission" do
+    post :create, :contact => {:name => "Test", :email => "test@example.com", :message => "Testing Message"}
+    assert_response :redirect
+  end
+
+
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,4 +4,14 @@ class PagesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Should include elements1" do
+    get :welcome
+    assert_select ".home .jumbotron"
+    assert :success
+  end
+
+  test "Should include elements" do
+    get :welcome
+    assert_select ".header-ul li", 5
+  end
 end
