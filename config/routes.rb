d89de47efe4ba94da24f978361a6ee8e048e7d29
Kevin Houk <kmhouk@gmail.com>
2015-06-20 12:09:59 -0400

    Added contact page and fixed broken img links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
 
-  
+  get '/portfolio' => 'pages#application'
+
+  get '/contact' => 'contacts#contact'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
